788ad74ad86b1208458ec938ebaa80041921cb92
Cristian Klein <cristian.klein@elastisys.com>
2020-11-09 10:01:39 +0100

    Disable infrastructure changes for now

--- apply-ck.sh
+++ apply-ck.sh
@@ -6,9 +6,10 @@ export CK8S_CONFIG_PATH=$(pwd)/aws_glasswall-test-ck
 export CK8S_CODE_PATH=$(pwd)/ck8s-cluster
 export CK8S_PGP_FP=65FC8FACF4484F052A84F64F7A02A1A19BBB5AC2
 
-make -C ck8s-cluster build
-./ck8s_linux_amd64 apply --cluster sc
-./ck8s_linux_amd64 apply --cluster wc
+# Disabled for now, breaks cluster
+#make -C ck8s-cluster build
+#./ck8s_linux_amd64 apply --cluster sc
+#./ck8s_linux_amd64 apply --cluster wc
 
 export CK8S_ENVIRONMENT_NAME=glasswall-test-ck
 export CK8S_CLOUD_PROVIDER=aws
