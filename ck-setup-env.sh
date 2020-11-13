23d80a2ec3436392f02a92eb1bdbde1095c744af
Cristian Klein <cristian.klein@elastisys.com>
2020-11-13 14:14:29 +0100

    Remove deprecated variables

--- ck-setup-env.sh
+++ ck-setup-env.sh
@@ -6,7 +6,4 @@ export CK8S_CONFIG_PATH=$(pwd)/aws_glasswall-test-ck
 export CK8S_CODE_PATH=$(pwd)/ck8s-cluster
 export CK8S_PGP_FP=65FC8FACF4484F052A84F64F7A02A1A19BBB5AC2
 
-export CK8S_ENVIRONMENT_NAME=glasswall-test-ck
-export CK8S_CLOUD_PROVIDER=aws
-
 popd
