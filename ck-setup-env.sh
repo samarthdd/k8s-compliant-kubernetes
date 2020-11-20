b9b25b84fe28045d10ad1d8f3bbfdf323c518727
Jakub Krzywda <jakub@elastisys.com>
2020-11-20 11:19:12 +0100

    Use environment variables to set domains

--- ck-setup-env.sh
+++ ck-setup-env.sh
@@ -6,4 +6,11 @@ export CK8S_CONFIG_PATH=$(pwd)/aws_glasswall-test-ck
 export CK8S_CODE_PATH=$(pwd)/ck8s-cluster
 export CK8S_PGP_FP=65FC8FACF4484F052A84F64F7A02A1A19BBB5AC2
 
+export GW_ICAP_ALLOWED_DOMAINS=gov.uk.ck.glasswall-ck8s-proxy.com,www.gov.uk.ck.glasswall-ck8s-proxy.com,assets.publishing.service.gov.uk.ck.glasswall-ck8s-proxy.com
+export GW_ICAP_ROOT_DOMAIN=ck.glasswall-ck8s-proxy.com
+export GW_ICAP_SUBFILTER_ENV=".gov.uk,.gov.uk.ck.glasswall-ck8s-proxy.com  .amazonaws.com,.amazonaws.com.gov.uk.ck.glasswall-ck8s-proxy.com"
+export GW_ICAP_ADDITIONAL_HOST_0=www.gov.uk.ck.glasswall-ck8s-proxy.com
+export GW_ICAP_ADDITIONAL_HOST_1=assets.publishing.service.gov.uk.ck.glasswall-ck8s-proxy.com
+export GW_ICAP_URL=gov.uk.ck.glasswall-ck8s-proxy.com
+
 popd
--- jk-setup-env.sh
+++ jk-setup-env.sh
@@ -8,4 +8,12 @@ export CK8S_PGP_FP=C23FDC9A49016B30199DDC86D1F9F9A9ABE5282B
 export CK8S_ENVIRONMENT_NAME=glasswall-test
 export CK8S_CLOUD_PROVIDER=aws
 
+
+export GW_ICAP_ALLOWED_DOMAINS=gov.uk.glasswall-ck8s-proxy.com,www.gov.uk.glasswall-ck8s-proxy.com,assets.publishing.service.gov.uk.glasswall-ck8s-proxy.com
+export GW_ICAP_ROOT_DOMAIN=glasswall-ck8s-proxy.com
+export GW_ICAP_SUBFILTER_ENV=".gov.uk,.gov.uk.glasswall-ck8s-proxy.com  .amazonaws.com,.amazonaws.com.gov.uk.glasswall-ck8s-proxy.com"
+export GW_ICAP_ADDITIONAL_HOST_0=www.gov.uk.glasswall-ck8s-proxy.com
+export GW_ICAP_ADDITIONAL_HOST_1=assets.publishing.service.gov.uk.glasswall-ck8s-proxy.com
+export GW_ICAP_URL=gov.uk.glasswall-ck8s-proxy.com
+
 popd
