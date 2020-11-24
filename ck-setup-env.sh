c0c0bd0f2404072536efb3f36ca2d47b8d0c3493
Jakub Krzywda <jakub@elastisys.com>
2020-11-24 09:23:49 +0100

    Add Jakub's PGP FP

--- ck-setup-env.sh
+++ ck-setup-env.sh
@@ -4,7 +4,7 @@ pushd $(dirname $BASH_SOURCE)
 
 export CK8S_CONFIG_PATH=$(pwd)/aws_glasswall-test-ck
 export CK8S_CODE_PATH=$(pwd)/ck8s-cluster
-export CK8S_PGP_FP=65FC8FACF4484F052A84F64F7A02A1A19BBB5AC2
+export CK8S_PGP_FP=65FC8FACF4484F052A84F64F7A02A1A19BBB5AC2,C23FDC9A49016B30199DDC86D1F9F9A9ABE5282B
 
 export GW_ICAP_ALLOWED_DOMAINS=gov.uk.ck.glasswall-ck8s-proxy.com,www.gov.uk.ck.glasswall-ck8s-proxy.com,assets.publishing.service.gov.uk.ck.glasswall-ck8s-proxy.com
 export GW_ICAP_ROOT_DOMAIN=ck.glasswall-ck8s-proxy.com
